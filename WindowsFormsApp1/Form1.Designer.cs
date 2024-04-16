4b6a92f614e286ba946bc488ce6989291d82dae7
William1505 <94123816+William1505@users.noreply.github.com>
2024-04-16 11:07:58 -0600

    Otro cambio

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -74,6 +74,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
